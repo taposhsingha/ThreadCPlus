e09dc8813a43710f5d9c52cfb8df7b3872955144
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-27 19:45:16 +0600

    added two functions

--- ThreadExample1.cpp
+++ ThreadExample1.cpp
@@ -2,7 +2,27 @@
 #include<thread>
 using namespace std;
 
-int main(){
-cout<<"My name"<<endl;
-return 0;
+unsigned long long int a=0,b=0;
+void firstTask()
+{
+    for(int i=0; i<1000000; i++)
+    {
+        a=a+i;
+    }
+    cout<< a << endl;
+}
+void secondTask()
+{
+    for(int j=11; j<2000000; j++)
+    {
+        b=b+j;
+    }
+    cout<< b << endl;
+}
+
+int main()
+{
+    firstTask();
+    secondTask();
+    return 0;
 }

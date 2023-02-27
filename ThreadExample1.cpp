611c85c134d936b9a155ec1ec8c2cd0fe5f80ade
taposhsingha <57071916+taposhsingha@users.noreply.github.com>
2023-02-27 20:34:53 +0600

    duration count for thread added

--- ThreadExample1.cpp
+++ ThreadExample1.cpp
@@ -1,11 +1,14 @@
 #include<iostream>
 #include<thread>
+#include<chrono>
+#include<algorithm>
 using namespace std;
+using namespace std::chrono;
 
 unsigned long long int a=0,b=0;
 void firstTask()
 {
-    for(int i=0; i<1000000000; i++)
+    for(int i=0; i<100000000; i++)
     {
         a=a+i;
     }
@@ -13,7 +16,7 @@ void firstTask()
 }
 void secondTask()
 {
-    for(int j=11; j<200000000; j++)
+    for(int j=11; j<20000000; j++)
     {
         b=b+j;
     }
@@ -22,9 +25,13 @@ void secondTask()
 
 int main()
 {
+    auto start = high_resolution_clock::now();
     std::thread t1(firstTask);
     std::thread t2(secondTask);
     t1.join();
     t2.join();
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << duration.count()/1000000<<endl;
     return 0;
 }
